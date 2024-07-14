485b6e81c9d37b98f97ec741d38b507eec179f3a
ganbin <jlau.ice@gmail.com>
2024-07-14 16:22:06 +0800

    数据库命名规范

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,24 +1,24 @@
 -- 创建库
-create database if not exists my_db;
+create database if not exists open_api;
 
 -- 切换库
-use open_api_mock;
+use open_api;
 
 -- 用户表
 create table if not exists user
 (
     id           bigint auto_increment comment 'id' primary key,
-    userName     varchar(256)                           null comment '用户昵称',
-    userAccount  varchar(256)                           not null comment '账号',
-    userAvatar   varchar(1024)                          null comment '用户头像',
+    user_name     varchar(256)                           null comment '用户昵称',
+    user_account  varchar(256)                           not null comment '账号',
+    user_avatar   varchar(1024)                          null comment '用户头像',
     gender       tinyint                                null comment '性别',
-    userRole     varchar(256) default 'user'            not null comment '用户角色：user / admin',
-    userPassword varchar(512)                           not null comment '密码',
-    createTime   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete     tinyint      default 0                 not null comment '是否删除',
-    constraint uni_userAccount
-        unique (userAccount)
+    user_role     varchar(256) default 'user'            not null comment '用户角色：user / admin',
+    user_password varchar(512)                           not null comment '密码',
+    create_time   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
+    update_time   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    is_delete     tinyint      default 0                 not null comment '是否删除',
+    constraint uni_user_account
+        unique (user_account)
 ) comment '用户';
 
 -- 帖子表
@@ -31,15 +31,15 @@ create table if not exists post
     place         varchar(512)                       null comment '地点',
     job           varchar(512)                       null comment '职业',
     contact       varchar(512)                       null comment '联系方式',
-    loveExp       varchar(512)                       null comment '感情经历',
+    love_exp       varchar(512)                       null comment '感情经历',
     content       text                               null comment '内容（个人介绍）',
     photo         varchar(1024)                      null comment '照片地址',
-    reviewStatus  int      default 0                 not null comment '状态（0-待审核, 1-通过, 2-拒绝）',
-    reviewMessage varchar(512)                       null comment '审核信息',
-    viewNum       int                                not null default 0 comment '浏览数',
-    thumbNum      int                                not null default 0 comment '点赞数',
-    userId        bigint                             not null comment '创建用户 id',
-    createTime    datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime    datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete      tinyint  default 0                 not null comment '是否删除'
+    review_status  int      default 0                 not null comment '状态（0-待审核, 1-通过, 2-拒绝）',
+    review_message varchar(512)                       null comment '审核信息',
+    view_num       int                                not null default 0 comment '浏览数',
+    thumb_num      int                                not null default 0 comment '点赞数',
+    user_id        bigint                             not null comment '创建用户 id',
+    create_time    datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    update_time    datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    is_delete      tinyint  default 0                 not null comment '是否删除'
 ) comment '帖子';
