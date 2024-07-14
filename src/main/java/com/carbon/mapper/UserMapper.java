85bde8f2e964e3538980d967b869cfe784f6f219
ganbin <jlau.ice@gmail.com>
2024-07-14 16:18:10 +0800

    init

---
+++ src/main/java/com/carbon/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.carbon.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.carbon.model.entity.User;
+
+/**
+ * @Entity com.yupi.project.model.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
